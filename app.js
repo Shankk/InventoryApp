0c0b93602fb77aa136834d28e31743abc7a0fbe5
Shankk <brianviveiros@hotmail.com>
2025-06-25 23:04:07 -0400

    Created initial folders and files for project setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Inventory App - listening on port ${PORT}!`);
+});
